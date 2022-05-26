83e5e9a0fc7d7e843507163157d875c6aba8cbcf
ikebeelion <102205223+ikebeelion@users.noreply.github.com>
2022-05-26 06:42:47 +0100

    Update Api.js

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let Api = axios.create({
-    baseURL: "https://akademiaapi.herokuapp.com/api",
+    baseURL: "https://akademiapowerfield.ng/api",
     headers: {
         Authorization: 'Bearer ' + localStorage.getItem('parenttoken') 
     },
